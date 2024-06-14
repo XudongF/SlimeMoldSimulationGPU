48d0b07e795ce54d81e4fe9984b7bad86cd01ca0
XudongFan <xudong.fan.tju@gmail.com>
2024-06-14 11:02:20 -0400

    update

--- scripts/Simulation_cuda.py
+++ scripts/Simulation_cuda.py
@@ -1,4 +1,3 @@
-
 # %%
 from Agent import Slime
 from numba import cuda
@@ -13,18 +12,40 @@ from numba.cuda.random import create_xoroshiro128p_states, xoroshiro128p_uniform
 def generate_sample(Diameter, radius):
     r = radius * np.sqrt(random.random())
     theta = random.random() * 2 * math.pi
-    x = Diameter/2 + r * math.cos(theta)
-    y = Diameter/2 + r * math.sin(theta)
+    x = Diameter / 2 + r * math.cos(theta)
+    y = Diameter / 2 + r * math.sin(theta)
     return np.array([x, y], dtype=np.int32)
 
 
-def generate_agents(diameter, boundaryControl, initial_speed, sensorDist, sensorSize, sensorAngle, maxTurnAngle, agent_number):
-    locations = [generate_sample(diameter, radius=int(
-        diameter/2 - boundaryControl)) for i in range(agent_number)]
+def generate_agents(
+    diameter,
+    boundaryControl,
+    initial_speed,
+    sensorDist,
+    sensorSize,
+    sensorAngle,
+    maxTurnAngle,
+    agent_number,
+):
+    locations = [
+        generate_sample(diameter, radius=int(diameter / 2 - boundaryControl))
+        for i in range(agent_number)
+    ]
     locations = np.unique(locations, axis=0)
 
-    slimes = [Slime(location=location, speed=initial_speed, angle=random.uniform(0, 2*math.pi),
-                    sensordistance=sensorDist, sensorSize=sensorSize, sensorAngle=sensorAngle, maxTurnAngle=maxTurnAngle, move=True) for location in locations]
+    slimes = [
+        Slime(
+            location=location,
+            speed=initial_speed,
+            angle=random.uniform(0, 2 * math.pi),
+            sensordistance=sensorDist,
+            sensorSize=sensorSize,
+            sensorAngle=sensorAngle,
+            maxTurnAngle=maxTurnAngle,
+            move=True,
+        )
+        for location in locations
+    ]
 
     return slimes
 
@@ -40,28 +61,52 @@ def draw_occupied(occupied, foodlayer=None):
     else:
         plot_matrix = occupied + 50
     plot_matrix = np.where(plot_matrix > 255, 255, plot_matrix)
-    img = Image.fromarray(plot_matrix.astype(np.uint8), 'L')
+    img = Image.fromarray(plot_matrix.astype(np.uint8), "L")
     return img
 
 
-def generate_Food(foodNumber, diameter, boundaryControl, foodWeight, mask, random_food=False, foodLocation=None):
+def generate_Food(
+    foodNumber,
+    diameter,
+    boundaryControl,
+    foodWeight,
+    mask,
+    random_food=False,
+    foodLocation=None,
+):
     if random_food:
         random.seed(foodNumber)
-        foodLocation = [generate_sample(diameter, radius=int(
-            diameter/2-2*boundaryControl)) for i in range(foodNumber)]
+        foodLocation = [
+            generate_sample(diameter, radius=int(diameter / 2 - 2 * boundaryControl))
+            for i in range(foodNumber)
+        ]
     else:
         foodLocation = foodLocation
 
-    foodlayer = getGaussianMap(mapSize=diameter, diffusionVariance=[
-        200], foodLocations=foodLocation, meanValue=foodWeight, mask=mask)
+    foodlayer = getGaussianMap(
+        mapSize=diameter,
+        diffusionVariance=[200],
+        foodLocations=foodLocation,
+        meanValue=foodWeight,
+        mask=mask,
+    )
 
-    foodlayer[foodlayer < 0.1*foodWeight] = 0
+    foodlayer[foodlayer < 0.1 * foodWeight] = 0
 
     return foodlayer
 
 
 @cuda.jit
-def one_step_simpulation(agent_location, petridish, angle, maxTurnAngle, distance, occupied, sensorAngle, rng_states):
+def one_step_simpulation(
+    agent_location,
+    petridish,
+    angle,
+    maxTurnAngle,
+    distance,
+    occupied,
+    sensorAngle,
+    rng_states,
+):
     start = cuda.grid(1)
     stride = cuda.gridsize(1)
 
@@ -69,35 +114,60 @@ def one_step_simpulation(agent_location, petridish, angle, maxTurnAngle, distanc
         F = 0
         for i in range(12):
             for j in range(12):
-                F += petridish[round(agent_location[agent_id][0] + distance *
-                                     math.sin(angle[agent_id]) + i), round(agent_location[agent_id][1] + distance *
-                                                                           math.cos(angle[agent_id]) + j)]
+                F += petridish[
+                    round(
+                        agent_location[agent_id][0]
+                        + distance * math.sin(angle[agent_id])
+                        + i
+                    ),
+                    round(
+                        agent_location[agent_id][1]
+                        + distance * math.cos(angle[agent_id])
+                        + j
+                    ),
+                ]
         FL = 0
         for i in range(12):
             for j in range(12):
-                FL += petridish[round(agent_location[agent_id][0] + distance *
-                                      math.sin(angle[agent_id]+sensorAngle) + i), round(agent_location[agent_id][1] + distance *
-                                                                                        math.cos(angle[agent_id]+sensorAngle) + j)]
+                FL += petridish[
+                    round(
+                        agent_location[agent_id][0]
+                        + distance * math.sin(angle[agent_id] + sensorAngle)
+                        + i
+                    ),
+                    round(
+                        agent_location[agent_id][1]
+                        + distance * math.cos(angle[agent_id] + sensorAngle)
+                        + j
+                    ),
+                ]
         FR = 0
         for i in range(12):
             for j in range(12):
-                FR += petridish[round(agent_location[agent_id][0] + distance *
-                                      math.sin(angle[agent_id]-sensorAngle) + i), round(agent_location[agent_id][1] + distance *
-                                                                                        math.cos(angle[agent_id]-sensorAngle) + j)]
+                FR += petridish[
+                    round(
+                        agent_location[agent_id][0]
+                        + distance * math.sin(angle[agent_id] - sensorAngle)
+                        + i
+                    ),
+                    round(
+                        agent_location[agent_id][1]
+                        + distance * math.cos(angle[agent_id] - sensorAngle)
+                        + j
+                    ),
+                ]
 
         if (F > FL) and (F > FR):
             angle[agent_id] = angle[agent_id] + 0
-        elif (FL < FR):
+        elif FL < FR:
             angle[agent_id] = angle[agent_id] - maxTurnAngle
-        elif (FL > FR):
+        elif FL > FR:
             angle[agent_id] = angle[agent_id] + maxTurnAngle
         else:
             angle[agent_id] = angle[agent_id] + 0
 
-        next_location_x = round(
-            agent_location[agent_id][0] + math.sin(angle[agent_id]))
-        next_location_y = round(
-            agent_location[agent_id][1] + math.cos(angle[agent_id]))
+        next_location_x = round(agent_location[agent_id][0] + math.sin(angle[agent_id]))
+        next_location_y = round(agent_location[agent_id][1] + math.cos(angle[agent_id]))
 
         if occupied[next_location_x, next_location_y] == 0:
 
@@ -105,8 +175,9 @@ def one_step_simpulation(agent_location, petridish, angle, maxTurnAngle, distanc
             agent_location[agent_id][1] = next_location_y
 
         else:
-            angle[agent_id] = xoroshiro128p_uniform_float32(rng_states, agent_id) * \
-                math.pi*2
+            angle[agent_id] = (
+                xoroshiro128p_uniform_float32(rng_states, agent_id) * math.pi * 2
+            )
 
 
 @cuda.jit
@@ -131,8 +202,7 @@ def update_occupied(agent_location, occupied):
     start = cuda.grid(1)
     stride = cuda.gridsize(1)
     for location in range(start, agent_location.shape[0], stride):
-        occupied[agent_location[location][0],
-                 agent_location[location][1]] = 255
+        occupied[agent_location[location][0], agent_location[location][1]] = 255
 
 
 @cuda.jit
@@ -140,35 +210,44 @@ def update_petridish(agent_location, petridish):
     start = cuda.grid(1)
     stride = cuda.gridsize(1)
     for location in range(start, agent_location.shape[0], stride):
-        petridish[agent_location[location][0],
-                  agent_location[location][1]] += 0.5
+        petridish[agent_location[location][0], agent_location[location][1]] += 0.5
 
 
 if __name__ == "__main__":
     # Env setting
     foodNumber = 9
     boundaryControl = 100
-    diffusionK = np.ones((3, 3))/9
+    diffusionK = np.ones((3, 3)) / 9
     hazardLocation = np.array([900, 1100], dtype=np.float32)
     hazardRange = 200
     withHazard = False
-    location = 'SiouxFalls'
+    location = "SiouxFalls"
     diameter, node_dict, _ = get_network(
-        f'data/TNTPFiles/{location}/{location}_node.tntp', boundaryControl)
+        f"data/TNTPFiles/{location}/{location}_node.tntp", boundaryControl
+    )
 
     # Agent setting
-    agent_number = int(0.01*0.25*3.15*diameter**2)
+    agent_number = int(0.01 * 0.25 * 3.15 * diameter**2)
     initial_speed = 1
     sensorDist = 64
     diffuseWeight = 5
     sensorSize = 16
-    sensorAngle = math.pi/4
-    maxTurnAngle = math.pi/3
-    slimes = generate_agents(diameter, boundaryControl, initial_speed,
-                             sensorDist, sensorSize, sensorAngle, maxTurnAngle, agent_number)
+    sensorAngle = math.pi / 4
+    maxTurnAngle = math.pi / 3
+    slimes = generate_agents(
+        diameter,
+        boundaryControl,
+        initial_speed,
+        sensorDist,
+        sensorSize,
+        sensorAngle,
+        maxTurnAngle,
+        agent_number,
+    )
 
     mask = create_circular_mask(
-        diameter, diameter, radius=int(diameter/2-boundaryControl))
+        diameter, diameter, radius=int(diameter / 2 - boundaryControl)
+    )
 
     petridish = generate_petridish(diameter=diameter)
 
@@ -181,7 +260,14 @@ if __name__ == "__main__":
 
     foodLocation = list(node_dict.values())
     foodlayer = generate_Food(
-        foodNumber, diameter, boundaryControl, 5, mask, random_food=False, foodLocation=foodLocation)
+        foodNumber,
+        diameter,
+        boundaryControl,
+        5,
+        mask,
+        random_food=False,
+        foodLocation=foodLocation,
+    )
 
     petridish = petridish + foodlayer
 
@@ -210,11 +296,25 @@ if __name__ == "__main__":
             occupied_frame.append(draw_occupied(occupied, foodlayer))
 
         one_step_simpulation[1024, 1024](
-            locations_device, petridish_device, angles_device, maxTurnAngle, sensorDist, occupied_device, sensorAngle, rng_states)
+            locations_device,
+            petridish_device,
+            angles_device,
+            maxTurnAngle,
+            sensorDist,
+            occupied_device,
+            sensorAngle,
+            rng_states,
+        )
 
         update_petridish[1024, 1024](locations_device, petridish_device)
         evaporate[blockspergrid, threadsperblock](petridish_device)
         s += 1
 
-    occupied_frame[0].save('results/agents.gif',
-                           format='GIF', append_images=occupied_frame[1:], save_all=True, duration=1, loop=0)
+    occupied_frame[0].save(
+        "results/agents.gif",
+        format="GIF",
+        append_images=occupied_frame[1:],
+        save_all=True,
+        duration=1,
+        loop=0,
+    )
